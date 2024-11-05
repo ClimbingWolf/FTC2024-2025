28c271e8bbadb8997463a9e1fc73722adf8653b4
ClimbingWolf <spamton@gmail.com>
2024-11-05 16:34:51 -0500

    sus amogus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/writtencode/BaseDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/writtencode/BaseDrive.java
@@ -9,11 +9,16 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import java.util.ArrayList;
+import org.ejml.dense.row.FMatrixComponent;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvInternalCamera;
 
 @TeleOp(name = "BaseDriveCode")
 @Config
@@ -22,6 +27,8 @@ public class BaseDrive extends LinearOpMode {
     Orientation lastAngles = new Orientation();
 
     public ArmServoHolder armController;
+
+    public CRServo clawServo;
 
     public static double bottomServoSet = 0;
     public static double topServoSet = 0;
@@ -44,7 +51,6 @@ public class BaseDrive extends LinearOpMode {
     public static double xReach = 0.1;
     public static double yReach = 0.1;
 
-    public Servo clawServo;
 
     public ArrayList<Servo> topServos = new ArrayList<>();
     public ArrayList<Servo> bottomServos = new ArrayList<>();
@@ -52,14 +58,16 @@ public class BaseDrive extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
         topServos.add(hardwareMap.servo.get("topRot"));
         bottomServos.add(hardwareMap.servo.get("bottomRot"));
-        clawServo = hardwareMap.servo.get("claw");
-        //armController = new ArmServoHolder(topServos, bottomServos);
-        //armController.addTopStart(0.95);
-        //armController.addBottomStart(0.265);
-        //armController.addTopEnd(0.5);
-        //armController.addBottomEnd(0.265);
+        clawServo = hardwareMap.crservo.get("claw");
+        armController = new ArmServoHolder(topServos, bottomServos);
+        armController.addTopStart(0.95);
+        armController.addBottomStart(0.265);
+        armController.addTopEnd(0.39);
+        armController.addBottomEnd(0.53);
         DcMotor bright = hardwareMap.dcMotor.get("bright");
         DcMotor bleft = hardwareMap.dcMotor.get("bleft");
         DcMotor fright = hardwareMap.dcMotor.get("fright");
@@ -86,13 +94,15 @@ public class BaseDrive extends LinearOpMode {
             lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
             firstAngle = lastAngles.firstAngle + offsetOrientation;
             //get the x and y position of the joystick relative to the player
-            y = FtcMath.rotateY(gamepad1.left_stick_x, gamepad1.left_stick_y, Math.toRadians(firstAngle));
-            x = FtcMath.rotateX(gamepad1.left_stick_x, gamepad1.left_stick_y, Math.toRadians(firstAngle));
+            y = FtcMath.rotateY(gamepad1.left_stick_x, -gamepad1.left_stick_y, Math.toRadians(firstAngle));
+            x = FtcMath.rotateX(gamepad1.left_stick_x, -gamepad1.left_stick_y, Math.toRadians(firstAngle));
+            //y = gamepad1.left_stick_y;
+            //x = gamepad1.left_stick_x;
             rx = gamepad1.left_trigger-gamepad1.right_trigger;
             // basic teleop movement code
-            bright.setPower((y+x-rx)*movementSpeedmultiplier * brightMult);
+            bright.setPower((y-x-rx)*movementSpeedmultiplier * brightMult);
             fleft.setPower((y-x+rx)*movementSpeedmultiplier * fleftMult);
-            fright.setPower((y-x-rx)*movementSpeedmultiplier * frightMult);
+            fright.setPower((y+x-rx)*movementSpeedmultiplier * frightMult);
             bleft.setPower((y+x+rx)*movementSpeedmultiplier * bleftMult);
             dashboard.sendTelemetryPacket(packet);
             if(gamepad1.a) {
@@ -101,6 +111,26 @@ public class BaseDrive extends LinearOpMode {
             if(gamepad1.b){
                 topServos.get(0).setPosition(topServoSet);
                 bottomServos.get(0).setPosition(bottomServoSet);
+            }
+            if(gamepad1.dpad_up && (Math.sqrt(Math.pow(xReach,2) + Math.pow(yReach+0.05,2)) < armController.legLength * 2)){
+                yReach+=0.05;
+            }
+            if(gamepad1.dpad_right && (Math.sqrt(Math.pow(xReach+0.05,2) + Math.pow(yReach,2)) < armController.legLength * 2)){
+                xReach+=0.05;
+            }
+            if(gamepad1.dpad_down && (Math.sqrt(Math.pow(xReach,2) + Math.pow(yReach-0.05,2)) < armController.legLength * 2)){
+                yReach-=0.05;
+            }
+            if(gamepad1.dpad_left && (Math.sqrt(Math.pow(xReach-0.05,2) + Math.pow(yReach,2)) < armController.legLength * 2)){
+                xReach-=0.05;
+            }
+            if(gamepad1.left_bumper){
+                clawServo.setPower(1);
+            }
+            else if(gamepad1.right_bumper){
+                clawServo.setPower(-1);
+            }else{
+                clawServo.setPower(0);
             }
             armController.outputServoPos();
 
